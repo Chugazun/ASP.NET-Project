f0276aaf138d3ba89c815329a9837d04ee2aef50
Samuel Silva <samuelrodolfos@gmail.com>
2020-01-17 16:42:48 -0300

    Delete Entity

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -18,12 +18,25 @@ namespace SalesWebMVC.Services
 
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList().OrderBy(s => s.Name).ToList();
+            return _context.Seller.OrderBy(s => s.Name).ToList();
         }
 
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            Seller obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            //_context.Remove(FindById(id));
             _context.SaveChanges();
         }
     }
